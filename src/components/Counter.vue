6b55150f33a03b58dd7c43d652932cccb2ffabab
htactive-huytd <huytd@htactive.com>
2019-11-21 15:11:00 +0700

    v-model setter getter
    muon bind du lieu 2 chieu dung v-model
    thi trong computed dung 2 bo get set
    de truyen toi action...
    
    Signed-off-by: htactive-huytd <huytd@htactive.com>

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,9 +2,9 @@
   <div>
     <button @click="increment">Increatement</button>
     <button @click="asyncdecrement">Increatement</button>
-    <hr>
-    <br>
-    <input type="text" :value="value" @input="updateValue">
+    <hr />
+    <br />
+    <input type="text" v-model="value"/>
     <p>Text test: {{value}}</p>
   </div>
 </template>
@@ -13,17 +13,18 @@
 import { mapActions, mapMutations } from "vuex";
 export default {
   computed: {
-    value(){
-      return this.$store.getters.value;
+    value: {
+      get() {
+        return this.$store.getters.value;
+      },
+      set(value) {
+        this.$store.dispatch("updateValue", value);
+      }
     }
   },
   methods: {
     ...mapActions(["increment", "asyncdecrement"]),
-    ...mapMutations(["decreatement"]),
-    updateValue(event){
-      // dispatch() cái mà ta có thể kích hoạt sự kiện trong actions
-      this.$store.dispatch('updateValue', event.target.value)
-    }
+    ...mapMutations(["decreatement"])
   }
 };
 </script>
