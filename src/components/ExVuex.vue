da118792f48d295f289c88862db001cf93d30f91
htactive-huytd <huytd@htactive.com>
2019-11-21 11:07:15 +0700

    center alone
    du lieu trong store thay doi thi tat ca cac component dieu thay doi
    
    Signed-off-by: htactive-huytd <huytd@htactive.com>

--- src/components/ExVuex.vue
+++ src/components/ExVuex.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Increate and Decreate</h1>
     <Result></Result>
+    <Other></Other>
     <Counter></Counter>
   </div>
 </template>
@@ -9,11 +10,13 @@
 <script>
 import Counter from './Counter'
 import Result from './Result'
+import Other from './Other'
 
 export default {
   components:{
     Counter,
-    Result
+    Result,
+    Other
   }
 }
 </script>
---
+++ src/components/Other.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>This is result of Other: {{result}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    result(){
+      return this.$store.state.result
+    }
+  },
+}
+</script>
